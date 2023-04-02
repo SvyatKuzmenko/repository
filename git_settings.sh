0a64f4e3c643bf729813e8b794b34b05a21c182a
Svyat <SvyatKuzmenko@email.com>
2023-04-02 18:17:21 +0300

    Auto deploy

--- git_settings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-  echo "Set git config"
-  git config --local core.autocrlf input
-  git config --local user.name "Svyat"
-  git config --local user.email "SvyatKuzmenko@email.com"
-  git config user.name
-  git config user.email
